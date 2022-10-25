778cd03740eceac18b05767ec0d8b89e090be950
rasszmhmmd <115336316+rasszmhmmd@users.noreply.github.com>
2022-10-25 21:01:38 +0800

    Delete Modul 3 directory

--- Modul 3/SOAL 2/Bahasa Python/PRAK202-2210817210028-Muhammad Aulia Rasyid.py
+++
@@ -1,6 +0,0 @@
-a = input("Masukkan Nilai Pertama : ")
-b = input("Masukkan Nilai Kedua : ")
-a = float(a)
-b = float(b)
-hasil =a + b
-print("Hasil dari penjumlahan nilai pertama \"%.2f\" dan nilai kedua \"%.1f\" adalah \"%.2f\""%(a , b , hasil))
